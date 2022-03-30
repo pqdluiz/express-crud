36fd29341602fb18fcf4da204c40d880772b4fa8
luizaoformigapqd <luizlima@garantti.com.br>
2022-03-30 15:40:09 -0300

    feat: 🎸 gar-02

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { database } from "./config/database";
 
 const app = express();
+
+database.run("omnion");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
